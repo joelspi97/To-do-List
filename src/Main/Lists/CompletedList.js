a89c285ae47b5e0d64e245912d8f8eaa04ff22b3
joel <joelspi97@gmail.com>
2021-10-06 17:56:38 -0300

    dnd

--- src/Main/Lists/CompletedList.js
+++ src/Main/Lists/CompletedList.js
@@ -1,10 +1,26 @@
 import React from 'react';
 
 function CompletedList(props) {
+    const drop = e => {
+        e.preventDefault();
+        const todo_id = e.dataTransfer.getData('todo_id');
+        const todo = document.getElementById(todo_id);
+        todo.style.display = 'block';
+        e.target.appendChild(todo);
+    }
+
+    const dragOver = e => {
+        e.preventDefault();
+    }
+
     return (
         <section className="list-section">
             <h2>Completed tasks</h2>
-            <ul className="list completed-list">
+            <ul 
+                className="list completed-list"
+                onDrop={drop}
+                onDragOver={dragOver} 
+            >
                 {props.children}
             </ul>
         </section>
@@ -12,3 +28,4 @@ function CompletedList(props) {
 }
 
 export { CompletedList };
+
--- src/Main/TodoItem/TodoItem.js
+++ src/Main/TodoItem/TodoItem.js
@@ -1,19 +1,37 @@
 import React from 'react';
 
-function TodoItem({ description, id, markCompleted, priority, deleteTodo, editTodo }) {
+function TodoItem({ description, id, markCompleted, priority, deleteTodo, editTodo, props }) {
     const [openDropdown, setOpenDropdown] = React.useState(false);
     function toggleDropdown() {
         setOpenDropdown(prevState => !prevState);
     }
 
+    const dragStart = e => {
+        const target = e.target;
+
+        e.dataTransfer.setData('card_id', target.id);
+
+        setTimeout(() => {
+        target.style.display = 'none';
+        }, 0);
+    }
+
+    const dragOver = e => {
+        e.preventPropagation();
+    }
+
     return (
         <li 
+            id={id}
+            draggable="true"
+            onDragStart={dragStart}
+            onDragOver={dragOver}
             className={`
-                todo-item 
-                ${(priority === "low") && "todo-item--low"}
-                ${(priority === "medium") && "todo-item--medium"}
-                ${(priority === "high") && "todo-item--high"}
-            `}>
+            todo-item 
+            ${(priority === "low") && "todo-item--low"}
+            ${(priority === "medium") && "todo-item--medium"}
+            ${(priority === "high") && "todo-item--high"}
+        `}>
             <button 
                 className="todo-item__btn" type="button"
                 onClick={() => markCompleted(id)} 
@@ -25,17 +43,17 @@ function TodoItem({ description, id, markCompleted, priority, deleteTodo, editTo
             </p>
             <button 
                 className={`
-                    todo-item__btn 
-                    ${openDropdown && "todo-item__btn--open"}
+                todo-item__btn 
+                ${openDropdown && "todo-item__btn--open"}
                 `} type="button"
                 onClick={() => toggleDropdown()}
             >
-                <span 
-                    className={`
-                        icon 
-                        edit-icon 
-                        ${openDropdown && "edit-icon--open"}
-                    `}></span>
+            <span 
+                className={`
+                icon 
+                edit-icon 
+                ${openDropdown && "edit-icon--open"}
+            `}></span>
             </button>
             <div className={`
                 todo-item__dropdown
@@ -44,8 +62,8 @@ function TodoItem({ description, id, markCompleted, priority, deleteTodo, editTo
                 <button
                     className="todo-item__btn todo-item__dropdown-option" 
                     onClick={() => {
-                        toggleDropdown();
-                        editTodo(id);
+                    toggleDropdown();
+                    editTodo(id);
                     }}
                 >
                     <span>Edit</span>
